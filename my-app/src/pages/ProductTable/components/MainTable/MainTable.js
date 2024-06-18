404882b1886b36f78fcd3421cda63d48b2bc8405
Pavlo Zhabort <zhabortp@gmail.com>
2024-06-18 14:37:34 +0300

    mokapi task done,fixed icons size

--- my-app/src/pages/ProductTable/components/MainTable/MainTable.js
+++ my-app/src/pages/ProductTable/components/MainTable/MainTable.js
@@ -1,37 +1,17 @@
+import React, { useState, useEffect } from "react";
 import "./MainTable.css";
 import PencilIcon from "../../../../assets/svg/PencilIcon";
 import TrashIcon from "../../../../assets/svg/TrashIcon";
 
 const MainTable = () => {
-  const data = [
-    {
-      id: 0,
-      category: "PC",
-      name: "Lenovo Y50-70",
-      quantity: 5,
-      price: "25,000.00",
-      icon: <PencilIcon />,
-      icon2: <TrashIcon />,
-    },
-    {
-      id: 1,
-      category: "Clothes",
-      name: "Nike M Nk Df Acd21",
-      quantity: 22,
-      price: "4,000.00",
-      icon: <PencilIcon />,
-      icon2: <TrashIcon />,
-    },
-    {
-      id: 2,
-      category: "Plumbing",
-      name: "CERSANIT MITO 17",
-      quantity: 1337,
-      price: "5,000.00",
-      icon: <PencilIcon />,
-      icon2: <TrashIcon />,
-    },
-  ];
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://6671686de083e62ee43b7889.mockapi.io/products/tableProducts")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
 
   return (
     <div className="table-block">
@@ -44,16 +24,24 @@ const MainTable = () => {
             <th>Name</th>
             <th>Quantity</th>
             <th>Price (₴)</th>
-            <th></th>
-            <th></th>
+            <th>Edit</th>
+            <th>Delete</th>
           </tr>
         </thead>
         <tbody>
           {data.map((row) => (
             <tr key={row.id}>
-              {Object.values(row).map((cell, index) => (
-                <td key={index}>{cell}</td>
-              ))}
+              <td>{row.id}</td>
+              <td>{row.category}</td>
+              <td>{row.name}</td>
+              <td>{row.quantity}</td>
+              <td>{row.price}</td>
+              <td>
+                <PencilIcon />
+              </td>
+              <td>
+                <TrashIcon />
+              </td>
             </tr>
           ))}
         </tbody>
